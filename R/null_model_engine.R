f2396b3b65cba083d3046443603e9c4d652413ef
Edmund Hart <edmund.m.hart@gmail.com>
2014-03-08 11:08:48 -0700

    Finished up draft of EcosimR with S3 classes.

--- R/null_model_engine.R
+++ R/null_model_engine.R
@@ -3,18 +3,35 @@
 #'@param species_data a dataframe <put some guidelines in here>
 #'@param algo the algorithm to use, must be "RA1", "RA2", "RA3", "RA4"
 #'@param metric the metric used to caluclate the null model: choices are "Pianka", "Czekanowski", "Pianka.var", "Czekanowski.var", "Pianka.skew", "Czekanowski.skew"; default is Pianka
-#'@param N.reps the number of replicates to run the null model for
+#'@param n.reps the number of replicates to run the null model.
+#'@param row.names Does your dataframe have row names? If yes, they are stripped, otherwise FALSE for data that has no row names
+#'@param random.seed Choose a seed to start your random number.  0 will choose a random seed, otherwise set the seed with any integer.
 #'@examples \dontrun{
-#' Put an example in here
+#' ## Load MacAruthur warbler data
+#' data(macwarb)
+#' 
+#' ## Run the null model
+#' warbMod <- null_model_engine(macwarb)
+#' ## Summary and plot info
+#' summary(warbMod)
+#' plot(warbMod)
 #'  
 #'}
 #'
 #'@export
 
-null_model_engine <- function(species_data, algo = "RA3", metric = "Pianka", N.Reps = 1000)
+null_model_engine <- function(species_data, algo = "RA3", metric = "Pianka", n.reps = 1000, row.names = TRUE, random.seed = 0)
 {
-
-
+  ## Set the seed
+  ifelse (random.seed==0, RandomInteger <- trunc(runif(1,-2000000000,2000000000)), RandomInteger <- random.seed)
+  
+  set.seed(RandomInteger)
+  
+  ### Strip out row names
+  
+  if(row.names){
+    species_data <- species_data[,-1] 
+  }
   algoF <- eval(parse(text = match.arg(algo,choices = c("RA1","RA2","RA3","RA4"))))
   metricF <- eval(parse(text = match.arg(metric,choices = c("Pianka", "Czekanowski", "Pianka.var", "Czekanowski.var", "Pianka.skew", "Czekanowski.skew"))))
   
@@ -22,28 +39,25 @@ null_model_engine <- function(species_data, algo = "RA3", metric = "Pianka", N.R
   Start.Time <- Sys.time()
   
   
-  if (N.Reps < 2) N.Reps <- 2
-  Sim <- replicate(N.Reps,metricF(algoF(species_data)))
+  if (n.reps < 2) n.reps <- 2
+  Sim <- replicate(n.reps,metricF(algoF(species_data)))
   Obs <- metricF(species_data)
   
   End.Time <- Sys.time()
   Elapsed.Time <- format(End.Time-Start.Time,digits=2)
   Time.Stamp <- date()
   
-  Null.Model.Out <- list(Obs=Obs,Sim=Sim, Elapsed.Time=Elapsed.Time, Time.Stamp=Time.Stamp,Metric = metric, Algorithm = algo)
+  Null.Model.Out <- list(Obs=Obs,Sim=Sim, Elapsed.Time=Elapsed.Time, Time.Stamp=Time.Stamp,Metric = metric, Algorithm = algo, N.Reps = n.reps, RandomInteger = RandomInteger)
   class(Null.Model.Out) <- "nichenullmod"
   return(Null.Model.Out)
   
 }
 
-#############################################
-## Null.Model.Summary function
-## Generic function for calculating null model summary statistics
-## Takes as input a list of Null.Model.Out, with Obs, Sim, Elapsed Time, and Time Stamp values
-##
-## Rewritten 24 January 2013 by AME to allow for nice screen or file output
-## Also eliminates need for Print.Results function
-##
+
+
+#' Generic function for calculating null model summary statistics
+#' @description Takes as input a list of Null.Model.Out, with Obs, Sim, Elapsed Time, and Time Stamp values
+#' @export
 
 summary.nichenullmod <- function(nullmodObj)
 { 
@@ -56,7 +70,7 @@ summary.nichenullmod <- function(nullmodObj)
   cat("Time Stamp: " , nullmodObj$Time.Stamp,   "\n") 
  # cat("Data File: ", p$Data.File,  "\n")
 #  cat("Output File: ", p$Output.File,  "\n") 
-  cat("Random Number Seed: ",RandomInteger,  "\n")
+  cat("Random Number Seed: ",nullmodObj$RandomInteger,  "\n")
   cat("Number of Replications: ",nullmodObj$N.Reps,  "\n")
   cat("Elapsed Time: ", nullmodObj$Elapsed.Time, "\n")
   cat("Metric: ", nullmodObj$Metric,  "\n")
@@ -64,14 +78,14 @@ summary.nichenullmod <- function(nullmodObj)
   
   cat("Observed Index: ", format(nullmodObj$Obs,digits=5),  "\n")
   cat("Mean Of Simulated Index: ",format(mean(nullmodObj$Sim),digits=5),  "\n")
-  cat("Variance Of Simulated Index: ",format(var(Sim),digits=5),  "\n")
+  cat("Variance Of Simulated Index: ",format(var(nullmodObj$Sim),digits=5),  "\n")
   cat("Lower 95% (1-tail): ",format(quantile(nullmodObj$Sim,0.05),digits=5),  "\n")
   cat("Upper 95% (1-tail): ",format(quantile(nullmodObj$Sim,0.95),digits=5), "\n")
   cat("Lower 95% (2-tail): ",format(quantile(nullmodObj$Sim,0.025),digits=5), "\n")
   cat("Upper 95% (2-tail): ",format(quantile(nullmodObj$Sim,0.975),digits=5),  "\n")
   
   #P-values
-  if (Obs > max(Sim)) {
+  if (nullmodObj$Obs > max(nullmodObj$Sim)) {
     cat("P(Obs <= null) < ",1/length(nullmodObj$Sim),  "\n")
     cat("P(Obs >= null) > ",(length(nullmodObj$Sim) - 1)/length(nullmodObj$Sim),  "\n")
   } else if (sum(nullmodObj$Obs <= nullmodObj$Sim)==0) {
@@ -86,4 +100,33 @@ summary.nichenullmod <- function(nullmodObj)
   cat("Standardized Effect Size (SES): ", format((nullmodObj$Obs - mean(nullmodObj$Sim))/sd(nullmodObj$Sim),digits=5), "\n")
   
   #if(!is.null(Output.File)) close(outfile)
+}
+
+
+
+
+#' Null Model Plot function
+#' @description Generic function for plotting a histogram of simulated values Takes as input a list of Null.Model.Out, with Obs and Sim values
+#' @export
+
+
+
+plot.nichenullmod <- function(nullmodObj)
+{
+  
+  
+  opar <- par(no.readonly=TRUE)
+  par(cex=1, cex.axis = 1.5,
+      cex.main=1,cex.lab=1.6)
+  par (mar=c(5,6,4,2)+0.1)
+  #------------------------------------------------------
+  hist(nullmodObj$Sim, breaks=20, col="royalblue3",
+       
+       xlab="Simulated Metric",ylab="Frequency",main="",
+       xlim=range(c(nullmodObj$Sim,nullmodObj$Obs)))
+  abline(v=nullmodObj$Obs,col="red",lty="solid",lwd=2.5)
+  abline(v=quantile(nullmodObj$Sim,c(0.05,0.95)),col="black",lty="dashed",lwd=2.5)
+  abline(v=quantile(nullmodObj$Sim,c(0.025,0.975)),col="black",lty="dotted",lwd=2.5)
+  mtext(as.character(date()),side=3,adj=1,line=3)
+  
 }
