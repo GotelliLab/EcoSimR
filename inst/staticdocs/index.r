daf1365c6f1355693ba49ad92f91bd299b8c09ac
Ted Hart <edmund_hart@apple.com>
2015-01-26 23:36:10 -0800

    Testing out staticdocs page, Issue #5

---
+++ inst/staticdocs/index.r
@@ -0,0 +1,8 @@
+sd_section(
+  "Settings functions",
+  "These functions are used in `index.r` to configure various
+    settings that staticdocs uses when rendering a package.  This is
+    particularly useful for generating an index page that groups functions
+    into useful categories",
+  c("sd_icon", "sd_item","sd_section")
+)
