c6bbdb646045f78bce674b4224ebaea5bae029c2
Ted Hart <edmund.m.hart@gmail.com>
2015-03-22 22:04:20 -0700

    testing out website doc build

--- inst/staticdocs/README.md
+++ inst/staticdocs/README.md
@@ -1,4 +1,9 @@
+## EcoSimR
+
 [![Build Status](https://travis-ci.org/GotelliLab/EcoSimR.svg?branch=master)](https://travis-ci.org/GotelliLab/EcoSimR)
 [![Build status](https://ci.appveyor.com/api/projects/status/fc5qpu8v3b1ku4ud?svg=true)](https://ci.appveyor.com/project/emhart/ecosimr)
 [![Coverage Status](https://coveralls.io/repos/GotelliLab/EcoSimR/badge.svg?branch=master)](https://coveralls.io/r/GotelliLab/EcoSimR?branch=master)
 [![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
+
+EcoSimR is a software package to fit null models to ecological data.  Currently three different types of models are supported: co-occurrence, niche overlap, and size ratio. The software comes with existing alogrithms and metrics, but individual users can also integrate their own algorithms, or use the null model engine to create new null models. If you publish using our software please cite it as:
+*insert citation here*
--- inst/staticdocs/index.R
+++ inst/staticdocs/index.R
@@ -6,11 +6,66 @@ sd_section("Niche overlap null models",
              "ra2",
              "ra3",
              "ra4",
+             "pianka",
+             "czekanowski",
+             "pianka_var",
+             "czekanowski_var",
+             "pianka_skew",
+             "czekanowski_skew"
+           )
+)
+
+sd_section("Size Ratio null models",
+           "Functions used for running size ratio null models",
+           c(
+             "size_null_model",
+             "size_uniform",
+             "size_uniform_user",
+             "size_source_pool",
+             "size_gamma",
+             "min_diff",
+             "min_ratio",
+             "var_diff",
+             "var_ratio"
+           )
+)
+
+sd_section("Co-Occurrence null models",
+           "Functions used for running co-occurrence null models",
+           c(
+             "cooc_null_model",
+             "vector_sample",
+             "sim1",
+             "sim2",
+             "sim3",
+             "sim4",
+             "sim5",
+             "sim6",
+             "sim7",
+             "sim8",
+             "sim9",
+             "sim10",
              "species_combo",
              "checker",
              "c_score",
              "c_score_var",
              "c_score_skew",
-             "v_ratio"
+             "v_ratio",
+             "checker"
+             
            )
 )
+
+sd_section("Data Sets",
+           "Data sets used in examples",
+           c(
+            "dataMacWarb",
+            "dataRodents",
+            "dataWiFinches",
+            "ranMatGen"
+           )
+)
+
+
+
+
